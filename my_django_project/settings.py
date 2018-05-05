d5f429533f93b091e4e83e1ce28a74b115602a8a
caiwen <caiwen@globalegrow.com>
2018-05-05 18:36:19 +0800

    修改项目的名称

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_project.urls'
+ROOT_URLCONF = 'my_django_project.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_project.wsgi.application'
+WSGI_APPLICATION = 'my_django_project.wsgi.application'
 
 
 # Database
